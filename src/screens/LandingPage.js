fbf04ee8437435fb2ec4fa546e42186a30a57f7f
Sanika <sanikaardekar@gmail.com>
2022-07-24 14:55:01 +0530

    landing page made responsive

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -7,29 +7,29 @@ import { Link } from 'react-router-dom';
 export default function LandingPage() {
     return (<>
         <Grid container className="page">
+            <div className="landing-root">
+                <Typography fontSize="25px" fontFamily="Nunito" fontWeight="700" color="#6776FF" marginTop="50px">
+                    Welcome to my Website 👋
+                </Typography>
 
-            <Typography fontSize="25px" fontFamily="Nunito" fontWeight="700" color="#6776FF" marginTop="50px">
-                Welcome to my Website 👋
-            </Typography>
-
-            <Typography fontSize="25px" fontFamily="Nunito" fontWeight="700" color="#6776FF">
-                Proceed to Fill the form 👇
-            </Typography>
-            <Link to="/page1" style={{textDecoration:"none"}}>
-            <Button
-                type="submit"
-                size="medium"
-                variant="contained"
-                sx={{ l: 10, mt: 2, mb: 1, backgroundColor: "#6776FF", color: "black", fontFamily: "Nunito", fontWeight: "700", }}
-                endIcon={<GrNext style={{ fill: "#6776FF" }} />}
-            >
-                Lets Start!
-            </Button>
-            </Link>
-            <Grid item sm={6} md={8} className="">
-                <img style={{ height: "50vh" }} src={Landing} alt="hello" />
-            </Grid>
-
+                <Typography fontSize="25px" fontFamily="Nunito" fontWeight="700" color="#6776FF">
+                    Proceed to Fill the form 👇
+                </Typography>
+                <Link to="/page1" style={{ textDecoration: "none" }}>
+                    <Button
+                        type="submit"
+                        size="medium"
+                        variant="contained"
+                        sx={{ l: 10, mt: 2, mb: 1, backgroundColor: "#6776FF", color: "black", fontFamily: "Nunito", fontWeight: "700", }}
+                        endIcon={<GrNext style={{ fill: "#6776FF" }} />}
+                    >
+                        Lets Start!
+                    </Button>
+                </Link>
+                <Grid item sm={6} md={8} className="">
+                    <img style={{ height: "50vh" }} className="landing" src={Landing} alt="hello" />
+                </Grid>
+            </div>
         </Grid>
     </>);
 }
